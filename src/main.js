357f45c54bb8789e8f372785185e5cfffb2ea4d0
Nikhil Musale <nikhil.musale1991@gmail.com>
2024-11-27 11:53:30 +0530

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginPage from './components/pages/LoginPage';
+import RegisterPage from './components/pages/RegisterPage';
+import DashboardPage from './components/pages/DashboardPage';
+  
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
+axios.interceptors.request.use(function (config) {
+  config.headers['X-Binarybox-Api-Key'] = process.env.VUE_APP_API_KEY;
+  return config;
+});
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: LoginPage },
+    { path: '/register', component: RegisterPage },
+    { path: '/dashboard', component: DashboardPage },
+  ],
+});
+  
+createApp(App).use(router).mount('#app');
